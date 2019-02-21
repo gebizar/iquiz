3e39e981ecb663368adb4fc27694bbfb856e31c8
Gabe <gebizar@uw.edu>
2019-02-21 02:18:12 -0800

    part 1 completed

---
+++ iquiz/iquiz/TableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  File.swift
+//  iquiz
+//
+//  Created by Gabe Bizar on 2/21/19.
+//  Copyright © 2019 Gabe Bizar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var subjects = ["Mathematics", "Marvel Super Heroes", "Science"]
+var subImNames = ["math", "marvel", "science"]
+var subjDesc = ["Test your algebraic, trignometric, and calculus knowledge!", "How super is your knowledge of pop superherodom?", "General Science Test!"]
+var currIndex = 0
+
+class TableViewController: UITableViewController {
+
+    @IBAction func settingsPress(_ sender: Any) {
+        let alert = UIAlertController(title: "Setting", message: "Settings go here", preferredStyle:.alert)
+        alert.addAction(UIAlertAction(title:"OK", style: .default, handler: {_ in NSLog("\"OK\" pressed.")}))
+        alert.addAction(UIAlertAction(title:"Cancel", style: .cancel, handler: {_ in NSLog("\"Cancel\" pressed.")}))
+        self.present(alert, animated: true, completion: {NSLog("The completion handler fired")})
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setToolbarHidden(false, animated: animated)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return subjects.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = subjects[indexPath.row]
+        let cellImage: UIImage = UIImage(named: subImNames[indexPath.row])!
+        cell.imageView?.image = cellImage
+        cell.detailTextLabel?.text = subjDesc[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        currIndex = indexPath.row
+        performSegue(withIdentifier: "segue", sender: self)
+    }
+}
---
+++ iquiz/iquiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iquiz
+//
+//  Created by Gabe Bizar on 2/20/19.
+//  Copyright © 2019 Gabe Bizar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
